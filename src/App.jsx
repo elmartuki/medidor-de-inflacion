2cbb3a881787f4e96e1580c9fc84b2683cb3bce9
Diaz Martin <elmartind17@gmail.com>
2025-11-12 14:21:33 -0300

    Primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import AppRouter from "./assets/routes/AppRouter";
+
+export default function App() {
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./assets/css/index.css";
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
+  // </StrictMode>
+);
