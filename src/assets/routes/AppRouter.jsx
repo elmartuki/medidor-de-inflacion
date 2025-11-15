34aa2817f7b535ceb6833794d97b83c5d72183aa
Diaz Martin <elmartind17@gmail.com>
2025-11-15 14:19:56 -0300

    Se arreglo un bug de importacion

--- src/assets/routes/AppRouter.jsx
+++ src/assets/routes/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import InitPage from "../pages/InitPage";
-import AdminPage from "../pages/adminPage";
+import AdminPage from "../../pages/adminPage.jsx";
 
 export default function AppRouter() {
   return (
