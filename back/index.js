fefafce5c6d329138f79b5f5ffa6a3601f1c4981
martuki <33644103+elmartuki@users.noreply.github.com>
2025-12-01 16:14:45 -0300

    Update allowed origins for CORS configuration

--- back/index.js
+++ back/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 
 const allowedOrigins = [
-  "https://inflacion-argy.vercel.app",
+  "https://micanastafrecuente.vercel.app/",
   "http://localhost:5173/",
 ];
 
