63bab8dbb6ffa7d15ffef3a6f9a3a983af9cbb23
Diaz Martin <elmartind17@gmail.com>
2025-12-01 19:35:11 -0300

    refactor(db-conection):Prueba de conexion 3.

--- back/index.js
+++ back/index.js
@@ -5,7 +5,10 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = ["https://micanastafrecuente.vercel.app"];
+const allowedOrigins = [
+  "https://micanastafrecuente.vercel.app",
+  "http://localhost:5173",
+];
 
 const corsOptions = {
   origin: allowedOrigins,
