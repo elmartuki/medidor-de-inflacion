96b7e8270e962017711428c877a76b5c062f6b12
Diaz Martin <elmartind17@gmail.com>
2025-11-30 23:47:12 -0300

    Refactor(db):Prueba de conexion 11

--- back/src/db/config-db.js
+++ back/src/db/config-db.js
@@ -4,18 +4,18 @@ const MONGODB_URI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
   if (!MONGODB_URI) {
-    console.error("Error: MONGO_URI no está definido.");
+    console.error(
+      "Error: La variable MONGO_URI no está configurada en Vercel."
+    );
 
     throw new Error("La URL de la base de datos no está configurada.");
   }
 
   try {
-    await mongoose.connect(MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("Conexión exitosa a MongoDB.");
   } catch (error) {
-    console.error("Error al conectar la base de datos:", error.message);
+    console.error("Error fatal al conectar la base de datos:", error.message);
 
     throw error;
   }
