91aabf0a8644771b33f739108a9995e78a2a1559
Diaz Martin <elmartind17@gmail.com>
2025-12-07 21:51:59 -0300

    feat(Skeleton-loading): Se agrego esta funcion para que cuando los productos esten cargando se muestre las tarjetas cargando.

---
+++ front/src/assets/constants/skeleton.js
@@ -0,0 +1,26 @@
+export const variacionesTitulosLoading = [
+  {
+    titulo: "Variación respecto hace 1 semana",
+  },
+  {
+    titulo: "Variación respecto hace 2 semanas",
+  },
+  {
+    titulo: "Variación respecto hace 3 semanas",
+  },
+  {
+    titulo: "Variación mensual",
+  },
+  {
+    titulo: "Variación respecto al 11/24",
+  },
+  {
+    titulo: "Producto que más aumentó:",
+  },
+  {
+    titulo: "Producto que más bajó:",
+  },
+  {
+    titulo: "Productos que subieron/bajaron",
+  },
+];
